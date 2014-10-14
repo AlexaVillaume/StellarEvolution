a267c31a3db79eb909d872bd84f7917246937412
Alexa Villaume <avillaum@ucsc.edu>
2014-10-13 23:03:25 -0700

    initial commit

---
+++ calc_density.py
@@ -0,0 +1,14 @@
+'''
+
+Problem Set 1, Problem 5.
+
+Develop a simple numerical program to calculate the density, given the pressure,
+temperature, and X, Y, and Z. Include radiation pressue and assume the gas is
+fully ionized. Evalute the density for:
+
+    (a) X=0.00, Y=0.98, log T = 7.55, log P = 16.85 (cgs)
+    (b) X=0.70, Y=0.28, log T = 6.91, log P = 16.87 (cgs)
+
+What is Beta, the ratio of gas pressure to total pressure in each case?
+
+'''
