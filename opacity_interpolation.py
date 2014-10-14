8511c93d5aa19a55bd6a428ce64136d21c12d326
Alexa Villaume <avillaum@ucsc.edu>
2014-10-13 23:07:51 -0700

    initial commit

---
+++ opacity_interpolation.py
@@ -0,0 +1,14 @@
+'''
+
+Develop a program, by interpolation in a table, for caculating the opacity of a
+gas with a given density and temperature, and with the chemical compoistion that you
+will use for the stellar model.
+
+What do you obtain for the opacity in (cm^2 g^-1) for:
+    (a) log T = 6.3 K, log row = 0.3 (cgs)
+    (a) log T = 5.0 K, log row = -4.0 (cgs)
+
+The temperature range you will need for the stellar models runs from 5x10^3 to 3x10^7 K,
+and the dnesity range from 10^-9 to 10^3 g cm^-3, depending ong T.
+
+'''
