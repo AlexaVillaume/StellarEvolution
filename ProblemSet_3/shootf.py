8d960de702e518caef9b1dccdcdf5793e4e68f51
Alexa Villaume <avillaum@ucsc.edu>
2014-11-09 12:37:08 -0800

    need to check on my convective/radiative condition and get all the constants in place. AND need to estimate pressure_c and temperature_c

--- ProblemSet_3/shootf.py
+++ ProblemSet_3/shootf.py
@@ -1,7 +1,8 @@
 '''
 '''
 import math
-import opacity_interpolation
+import ../StellarEvolutionCode/opacity_interpolation
+import ../StellarEvolutionCode/calc_density
 
 
 """
@@ -9,9 +10,11 @@ Constants
 """
 G =
 c =
-rho_c =
+# chatper 2.3
 pressure_c =
 temperature_c =
+total_lum =
+total_radius =
 mass_initial =
 
 def load1(mass, radius, luminosity):
@@ -28,9 +31,14 @@ def load1(mass, radius, luminosity):
     term3 = mass**(2/3)
     pressure = term1*term2*term3 + pressure_c
     # Test whether the gas is raidative
-    # or convective
-    if convective:
-        temperature =
+    # or convective by using the
+    density_c = calc_density.density_is(math.log10(temperature_c, pressure_c, X, Y))
+    opacity_c = opacity_interpolation.opacity_s(temperature_c, density_c)
+    # Need to check on on the conditions
+    if opacity_c < (2./5.):
+        term1 = -(math.pi/6)**(1/3)
+        term2 =
+        temperature = math.exp(term1*G*term2*mass**(2/3) - math.log(temperature_c))
     else:
         term1 = -1/(2*a*c)
         term2 = (3/4*math.pi)**(2/3)
