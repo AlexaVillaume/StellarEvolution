8f50b8f0b71c1edcb7ae16266ddf61f38738839c
Alexa Villaume <avillaum@ucsc.edu>
2014-11-18 13:18:06 -0800

    adding some object methods, should only be funtions specifc to the input values.

--- StellarEvolutionCode/make_star.py
+++ StellarEvolutionCode/make_star.py
@@ -4,7 +4,7 @@ This will be the control program for running the stellar evolution code.
 
 
 class star(object):
-    def __init__(core_pressure, core_temp, total_lum, total_radius, total_mass, hydrogen_mass, helium_mass):
+    def __init__(self, core_pressure, core_temp, total_lum, total_radius, total_mass, hydrogen_mass, helium_mass):
         """
         Units:
         Core Pressure:    g cm^-2
@@ -19,3 +19,10 @@ class star(object):
         self.total_radius = total_radius
         self.total_mass = total_mass
         self.hydrogen_mass = hydrogen_mass
+        self.helium_mass = helium_mass
+
+    def calc_teff(self):
+        return  (self.total_luminosity/(4*math.pi*sigma*self.total_radius**2))**(1./4.)
+
+# All values for the Sun
+solar = star(2.526e14, 1.57e7, 3.846e33, 7e10, 1.98e33, 0.70, 0.27)
